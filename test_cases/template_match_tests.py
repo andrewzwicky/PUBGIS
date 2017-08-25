37985b055ca99342ae9a2fff71760a85cb27a38e
Andrew Zwicky <andrew.zwicky@gmail.com>
2017-08-25 15:12:43 -0500

    updating test argument list

--- test_cases/template_match_tests.py
+++ test_cases/template_match_tests.py
@@ -10,15 +10,25 @@ _, PLAYER_INDICATOR_MASK = cv2.threshold(PLAYER_INDICATOR_IMG, 10, 255, cv2.THRE
 
 @pytest.mark.parametrize("test_image", os.listdir('bad'))
 def test_bad_images(test_image):
-    coords_found, coords = template_match_minimap(cv2.imread(os.path.join('bad', test_image)),
-                                                  GRAY_MAP,
-                                                  PLAYER_INDICATOR_MASK)
-    assert not coords_found
+    match_found,\
+    max_val,\
+    coords,\
+    ind_color,\
+    ind_in_range,\
+    minimap = template_match_minimap(cv2.imread(os.path.join('bad', test_image)),
+                                     GRAY_MAP,
+                                     PLAYER_INDICATOR_MASK)
+    assert not match_found
 
 
 @pytest.mark.parametrize("test_image", os.listdir('good'))
 def test_good_images(test_image):
-    coords_found, coords = template_match_minimap(cv2.imread(os.path.join('good', test_image)),
-                                                  GRAY_MAP,
-                                                  PLAYER_INDICATOR_MASK)
-    assert coords_found
+    match_found,\
+    max_val,\
+    coords,\
+    ind_color,\
+    ind_in_range,\
+    minimap = template_match_minimap(cv2.imread(os.path.join('good', test_image)),
+                                     GRAY_MAP,
+                                     PLAYER_INDICATOR_MASK)
+    assert match_found
