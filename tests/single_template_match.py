184b17124d277a927cf1853c58df4a75b06d026e
Andrew Zwicky <andrew.zwicky@gmail.com>
2017-08-31 17:31:04 -0500

    Updated incorrect test file name

--- tests/single_template_match.py
+++ tests/single_template_match.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     match = PUBGIS(full_map_file=r"../full_map_scaled.jpg",
                    mask_file=r"../player_indicator_mask.jpg",
                    debug=True)
-    for test_image in [r"good\shroud_2_36000_2602_2425.jpg",
+    for test_image in [r"good\shroud_2_33300_2400_2636.jpg",
                        r"good\shroud_2_36900_2603_2353.jpg"]:
         mf, coords = match.template_match(cv2.imread(test_image))
         print(coords)
