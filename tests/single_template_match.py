fd956a0829ae29bef9a665b01a2b17a3fa8b1686
Andrew Zwicky <andrew.zwicky@gmail.com>
2017-09-02 23:51:14 -0500

    Updating ignore files

--- tests/single_template_match.py
+++ tests/single_template_match.py
@@ -5,8 +5,7 @@ if __name__ == "__main__":
     match = PUBGIS(full_map_file=r"../full_map_scaled.jpg",
                    mask_file=r"../player_indicator_mask.jpg",
                    debug=True)
-    for test_image in [r"good\shroud_2_33300_2400_2636.jpg",
-                       r"good\shroud_2_36900_2603_2353.jpg"]:
+    for test_image in [r"bad\squads_dinner_mike_pat_11885.jpg"]:
         mf, coords = match.template_match(cv2.imread(test_image))
         print(coords)
         cv2.waitKey(-1)
