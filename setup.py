879a93e940be4f4be1fd4e84de28093988280548
Andrew Zwicky <andrew.zwicky@gmail.com>
2017-09-10 11:48:05 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages=[], excludes=[])
+
+import sys
+
+base = 'Win32GUI' if sys.platform == 'win32' else None
+
+executables = [
+    Executable('pubgis\pubgis_gui.py', base=base, targetName='PUBGIS')
+]
+
+setup(name='PUBGIS',
+      version='0.1',
+      description='',
+      options=dict(build_exe=buildOptions),
+      executables=executables)
