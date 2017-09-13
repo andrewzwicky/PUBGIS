71aea3f796e9d433ad2be9f293b09af871ead9a1
Andrew Zwicky <andrew.zwicky@gmail.com>
2017-09-13 01:24:51 -0500

    Updating argument names in tests

--- tests/test_template_match.py
+++ tests/test_template_match.py
@@ -1,5 +1,6 @@
 import os
 import re
+from math import sqrt
 
 import cv2
 import matplotlib.patches as patches
@@ -12,7 +13,7 @@ from pubgis.match import PUBGISMatch, MatchResult, COLOR_DIFF_THRESH_1,\
 
 GOOD_TEST_COORDS_RE = re.compile(r".*_\d+_(\d+)_(\d+)\.jpg")
 ALLOWED_VARIATION = 2  # pixels
-MAX_COLOR_DIFF = 450  # approx sqrt(255**2 + 255**2 + 255**2), between white and black
+MAX_COLOR_DIFF = int(sqrt(255**2 + 255**2 + 255**2)) # diff between white and black
 
 
 @pytest.fixture(scope='module')
@@ -28,7 +29,7 @@ def template_match_plot_axes():
     for color_diff, match_val, c in points_list:
         ax.scatter(color_diff, match_val, facecolor=c, edgecolor="none", s=10, alpha=0.2)
     ax.set_ylim(0, 1)
-    ax.set_xlim(left=0)
+    ax.set_xlim(0, MAX_COLOR_DIFF)
 
     ax.add_patch(patches.Rectangle((0, 0),
                                    MAX_COLOR_DIFF,
@@ -78,8 +79,8 @@ def map_coverage_axes():
 @pytest.mark.parametrize("test_image", os.listdir(r'bad'))
 def test_bad_images(test_image, match_fixture, template_match_plot_axes):
     img = cv2.imread(os.path.join(r'bad', test_image))
-    match_found, _, color_diff, match_val, _ = match_fixture.find_map_section((None, img))
-    template_match_plot_axes.append((color_diff, match_val, 'r'))
+    match_found, (found_x, found_y), color_diff, result, this_percent = match_fixture.find_map_section((None, img))
+    template_match_plot_axes.append((color_diff, result, 'r'))
     assert match_found != MatchResult.SUCCESFUL
 
 
@@ -87,10 +88,10 @@ def test_bad_images(test_image, match_fixture, template_match_plot_axes):
 @pytest.mark.parametrize("test_image", os.listdir(r'good'))
 def test_good_images(test_image, match_fixture, template_match_plot_axes, map_coverage_axes):
     img = cv2.imread(os.path.join(r'good', test_image))
-    match_found, (f_x, f_y), color_diff, match_val, _ = match_fixture.find_map_section((None, img))
-    template_match_plot_axes.append((color_diff, match_val, 'g'))
-    map_coverage_axes.add_patch(patches.Rectangle((f_x - (MMAP_WIDTH // 2),
-                                                   f_y - (MMAP_HEIGHT // 2)),
+    match_found, (found_x, found_y), color_diff, result, this_percent = match_fixture.find_map_section((None, img))
+    template_match_plot_axes.append((color_diff, result, 'g'))
+    map_coverage_axes.add_patch(patches.Rectangle((found_x - (MMAP_WIDTH // 2),
+                                                   found_y - (MMAP_HEIGHT // 2)),
                                                   MMAP_WIDTH,
                                                   MMAP_HEIGHT,
                                                   edgecolor="none",
@@ -102,6 +103,6 @@ def test_good_images(test_image, match_fixture, template_match_plot_axes, map_co
     else:
         (e_x, e_y) = (None, None)
 
-    assert (match_found, f_x, f_y) == (MatchResult.SUCCESFUL,
-                                       pytest.approx(e_x, abs=ALLOWED_VARIATION),
-                                       pytest.approx(e_y, abs=ALLOWED_VARIATION))
+    assert (match_found, found_x, found_y) == (MatchResult.SUCCESFUL,
+                                               pytest.approx(e_x, abs=ALLOWED_VARIATION),
+                                               pytest.approx(e_y, abs=ALLOWED_VARIATION))
