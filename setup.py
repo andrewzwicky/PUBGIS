7ab4db276558b9fb5173ed2ce022bff2be239aa9
Andrew Zwicky <andrew.zwicky@gmail.com>
2017-09-18 23:22:57 -0500

    Preparing for pypi hosting

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='PUBGIS',
-      version='0.1',
+      version='0.0.1',
       description='PUBG Location Tracker',
       author='Andrew Zwicky',
       url='https://github.com/andrewzwicky/PUBGIS',
