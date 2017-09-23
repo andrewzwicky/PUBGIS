210d8177af4d46bc91fd9596ccac7c96d8309a4f
Andrew Zwicky <andrew.zwicky@gmail.com>
2017-09-23 16:17:51 -0500

    Updating setup.py for V0.0.2

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from distutils.core import setup
 
 setup(name='PUBGIS',
-      version='0.0.1',
+      version='0.0.2',
       description='PUBG Location Tracker',
       author='Andrew Zwicky',
       author_email='andrew.zwicky@gmail.com',
+      license='GPLv3',
       url='https://github.com/andrewzwicky/PUBGIS',
       packages=['pubgis'],
       package_dir={'pubgis': 'pubgis'},
-      package_data={'pubgis': ['images/*.jpg']}
+      package_data={'pubgis': ['images/*.jpg']},
+      python_requires='>=3.6',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: X11 Applications :: Qt'
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Topic :: Games/Entertainment :: First Person Shooters',
+          'Topic :: Multimedia :: Graphics :: Capture :: Screen Capture',
+          'Topic :: Multimedia :: Video :: Capture',
+          'Topic :: Utilities',
+          'Programming Language :: Python :: 3.6',
+      ]
       )
