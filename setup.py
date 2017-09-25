059ba4c18f4d9092e07493df721695f4b967910c
Andrew Zwicky <andrew.zwicky@gmail.com>
2017-09-24 21:29:22 -0500

    Upping to v0.0.7 to test pypi script

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='PUBGIS',
-      version='0.0.6',
+      version='0.0.7',
       description='PUBG Location Tracker',
       author='Andrew Zwicky',
       author_email='andrew.zwicky@gmail.com',
