c6ea6f312f1dc9c38fe589ec0d6339361d4aec92
Andrew Zwicky <andrew.zwicky@gmail.com>
2017-09-26 10:50:57 -0500

    Updates to water_test to skip for the time being

--- tests/test_water_path.py
+++ tests/test_water_path.py
@@ -1,14 +1,23 @@
+from os.path import join, dirname
+
 import cv2
+import pytest
 
 from pubgis.image_iterator import ImageIterator
 from pubgis.match import PUBGISMatch
 
+WATER_IMAGES_FOLDER = join(dirname(__file__), "water_test")
 
+
+@pytest.mark.skip(reason="test not finished")
 def test_water_path():
-    image_iter = ImageIterator("water_test")
+    image_iter = ImageIterator(WATER_IMAGES_FOLDER)
     match = PUBGISMatch(minimap_iterator=image_iter)
 
     for percent, minimap in match.process_match():
-        cv2.imshow("test", cv2.resize(minimap, (0,0), fx=600/minimap.shape[0], fy=600/minimap.shape[0]))
+        cv2.imshow("test", cv2.resize(minimap,
+                                      (0, 0),
+                                      fx=600 / minimap.shape[0],
+                                      fy=600 / minimap.shape[0]))
         cv2.waitKey(10)
         print(f"{percent:2.2}")
