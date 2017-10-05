6ea29b011046739336997193d62fdfa26ba0813a
Andrew Zwicky <andrew.zwicky@gmail.com>
2017-10-04 22:15:17 -0500

    Removing debugging plotting from tests

--- tests/test_find_map_section.py
+++ tests/test_find_map_section.py
@@ -33,51 +33,11 @@ def good_match_fixture():
     return PUBGISMatch(good_iter)
 
 
-@pytest.fixture(scope='module')
-def template_match_plot_axes():
-    fig, axis = plt.subplots(figsize=(12, 10))
-    points_list = []
-    yield points_list
-    for color_diff, match_val, color in points_list:
-        axis.scatter(color_diff, match_val, facecolor=color, edgecolor="none", s=10, alpha=0.2)
-    axis.set_ylim(0, 1)
-    axis.set_xlim(0, MAX_COLOR_DIFF)
-
-    # TODO: explain what's happening here
-    x_coords = [val for val in COLOR_DIFF_THRESHS + [MAX_COLOR_DIFF] for _ in (0, 1)]
-    z_coords = [val for val in COLOR_DIFF_THRESHS + [0] for _ in (0, 1)]
-    y_coords = [1] + [val for val in TEMPLATE_MATCH_THRESHS for _ in (0, 1)] + [1]
-
-    axis.add_patch(
-        Polygon(list(zip(x_coords, y_coords)), alpha=0.1, edgecolor='none', facecolor='g'))
-    axis.add_patch(
-        Polygon(list(zip(z_coords, y_coords)), alpha=0.1, edgecolor='none', facecolor='r'))
-    axis.add_patch(Rectangle((0, 0),
-                             MAX_COLOR_DIFF,
-                             min(TEMPLATE_MATCH_THRESHS),
-                             edgecolor="none",
-                             facecolor='r',
-                             alpha=0.1))
-    fig.savefig("summary_plot.png")
-
-
-@pytest.fixture(scope='module')
-def map_coverage_axes(good_match_fixture):
-    fig, axis = plt.subplots(figsize=(10, 10))
-    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
-    axis.axes.xaxis.set_visible(False)
-    axis.axes.yaxis.set_visible(False)
-    axis.imshow(cv2.cvtColor(good_match_fixture.map, cv2.COLOR_BGR2RGB))
-    yield axis
-    fig.savefig("map_coverage.png")
-
-
 # noinspection PyShadowingNames
 @pytest.mark.parametrize("test_image", os.listdir(BAD_IMAGES_FOLDER))
 def test_bad_images(test_image, bad_match_fixture, template_match_plot_axes):
     img = cv2.imread(os.path.join(BAD_IMAGES_FOLDER, test_image))
     scaled_pos, color_diff, result = bad_match_fixture.find_scaled_player_position(img)
-    template_match_plot_axes.append((color_diff, result, 'r'))
     assert scaled_pos is None
 
 
@@ -85,20 +45,9 @@ def test_bad_images(test_image, bad_match_fixture, template_match_plot_axes):
 @pytest.mark.parametrize("test_image", os.listdir(GOOD_IMAGES_FOLDER))
 def test_good_images(test_image, good_match_fixture, template_match_plot_axes, map_coverage_axes):
     img = cv2.imread(os.path.join(GOOD_IMAGES_FOLDER, test_image))
-    img_height, img_width = img.shape[:2]
     scaled_pos, color_diff, result = good_match_fixture.find_scaled_player_position(img)
-    template_match_plot_axes.append((color_diff, result, 'g'))
-    map_coverage_axes.add_patch(Rectangle((f_x - (img_width // 2), f_y - (img_height // 2)),
-                                          img_width,
-                                          img_height,
-                                          edgecolor="none",
-                                          facecolor='white',
-                                          alpha=0.1))
     coords_match = GOOD_TEST_COORDS_RE.match(test_image)
-    if coords_match is not None:
-        (e_x, e_y) = tuple(map(int, coords_match.groups()))
-    else:
-        (e_x, e_y) = (None, None)
+    (e_x, e_y) = tuple(map(int, coords_match.groups()))
 
     assert scaled_pos == (pytest.approx(e_x, abs=ALLOWED_VARIATION),
                           pytest.approx(e_y, abs=ALLOWED_VARIATION))
